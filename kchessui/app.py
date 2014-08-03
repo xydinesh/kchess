2633052e4ae92a1261044a0ebddddaab0e870adf
Dinesh Weerapurage <xydinesh@gmail.com>
2014-08-03 15:28:20 -0400

    adding chanegs from previous implementation to current branch

--- kchessui/app.py
+++ kchessui/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import redis
+import os
+from datetime import datetime
+from time import strftime
+from pytz import timezone
+
+redis_addr = os.environ.get('SERVER_PORT_6379_TCP_ADDR', '192.168.59.103')
+redis_port = os.environ.get('SERVER_PORT_6379_TCP_PORT', 6379)
+
+r = redis.Redis(host=redis_addr, port=redis_port, db=3)
 
 # If you get an error on the next line on Python 3.4.0, change to: Flask('app')
 # where app matches the name of this file without the .py extension.
--- kchessui/routes.py
+++ kchessui/routes.py
@@ -29,3 +29,25 @@ def about():
         message = 'Your application description page.'
     )
 
+@app.route('/summary')
+def summary():
+    games = r.lrange('knoxville-games-list', 0, 100)
+    print games
+    return render_template('summary.html', games=list(games))
+
+@app.route('/game/result', methods=['POST'])
+def result():
+    data = []
+    fmt = "%Y-%m-%d %H:%M"
+    now_time = datetime.now(timezone('US/Eastern'))
+    data.append(now_time.strftime(fmt))
+    data.append(request.form['white'])
+    data.append(request.form['black'])
+    data.append(request.form['result'])
+    data.append(request.form['wtime'])
+    data.append(request.form['btime'])
+    data.append(request.form['comments'])
+    st = '|'.join(data)
+    r.rpush('knoxville-games-list', st)
+    return "OK"
+
