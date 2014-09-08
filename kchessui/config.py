102e76b27d68279043817dcc8da7050e71255e3e
Dinesh Weerapurage <xydinesh@gmail.com>
2014-09-08 16:56:55 -0400

    adding requirements.txt

--- kchessui/config.py
+++
@@ -1,5 +0,0 @@
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess asastaarsetiona189714-347)*(*08-a8 rst-0a8 dinesh'
-SQLALCHEMY_DATABASE_URI = 'postgresql://docker:docker@192.168.59.103:5432/docker'
-OPENID_PROVIDERS = [
-   ]
