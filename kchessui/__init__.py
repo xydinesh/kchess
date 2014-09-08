ac9913d73e8543dd8b8421d6e87c70c8e288b731
Dinesh Weerapurage <xydinesh@gmail.com>
2014-09-08 16:11:00 -0400

    publishing to azure

--- kchessui/__init__.py
+++ kchessui/__init__.py
@@ -5,8 +5,8 @@ from flask.ext.login import LoginManager, login_required, login_user, logout_use
 # If you get an error on the next line on Python 3.4.0, change to: Flask('app')
 # where app matches the name of this file without the .py extension.
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://docker:docker@192.168.59.103:5432/docker'
-app.config['SECRET_KEY'] ='xxxxxxxxarseintaorsnetia astaorsitnarosetni'
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
